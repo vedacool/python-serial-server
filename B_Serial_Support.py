bf0a43c4984f7d608bb8a8c399d66149cab1c07b
vedacool <hus6012@gmail.com>
2018-07-17 00:19:57 +0800

    Initial

--- B_Serial_Support.py
+++ B_Serial_Support.py
@@ -7,7 +7,10 @@ ser = serial.Serial(None, 9600)
 
 def initialize(Comm_P, Baud_Rate):
     global ser
+    # Linux COMM Addressing setting
     # ser = serial.Serial("/dev/ttyUSB0", Baud_Rate, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, None)
+
+    # Windows COMM Addressing setting
     ser = serial.Serial(Comm_P, Baud_Rate, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, None)
     return ser
 
